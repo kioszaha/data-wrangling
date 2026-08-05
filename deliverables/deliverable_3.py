0a56bc42d069bb78e502fe70456ac7b3d893feb4
Oskar <oskargreig@gmail.com>
2026-08-05 12:07:22 +1200

    Complete tasks 3-5

--- deliverables/deliverable_3.py
+++ deliverables/deliverable_3.py
@@ -25,11 +25,13 @@ def main():
     # Task 2: Load one dataset into Python or R
     june_listings = airbnb_listings.by_month(6)
 
-    # Task 3: Add a column for the month + year
+    # Task 3: Filter by Christchurch only
     june_listings.filter_christchurch()
+
+    # Task 4: Add a column for the month + year
     june_listings.prepare()
 
-    # Task 4: Do the same for all the other datasets, and concatenate them
+    # Task 5: Do the same for all the other datasets, and concatenate them
     airbnb_listings.filter_christchurch_all()
     airbnb_listings.prepare_all()
     combined_dataset = combine_datasets(airbnb_listings)
